artus-analysis/Artus

commit c0d37abe5b06f1326df167e0345f397f3e26273c

roottools.py: restructured friend tree functionality and sychronised argument handling with the one for the normal inputs

HarryPlotter/python/input_modules/inputroot.py
@@ -28,12 +28,16 @@ class InputRoot(inputfile.InputFile):
 	def modify_argument_parser(self, parser, args):
 		super(InputRoot, self).modify_argument_parser(parser, args)
 		
+		self.input_options.add_argument("--friend-files", type=str, nargs="+",
+		                                help="Filenames to be added as friends. Filenames separated by whitespaces in one argument will be added to the same friend tree.", default=[None])
+		
 		self.input_options.add_argument("-f", "--folders", type=str, nargs='*',
 		                                help="Path(s) to ROOT objects.")
-		self.input_options.add_argument("--friend-treenames", type=str, nargs="+",
-		                                help="Names of trees to be used as friends. Seperate different plots with space, seperate for same plot with whitespace.", default=None)
-		self.input_options.add_argument("--friend-filenames", type=str, nargs="+",
-		                                help="Filenames to be added as friends. Seperate different plots with space, seperate for same plot with whitespace.", default=None)
+		self.input_options.add_argument("--friend-folders", type=str, nargs="+",
+		                                help="Paths to trees to be added as friends. Paths separated by whitespaces in one argument will be added to the same friend tree. Currently, each input can only have one friend, although ROOT allows for more friends.", default=[None])
+		self.input_options.add_argument("--friend-aliases", type=str, nargs="+",
+		                                help="Aliases for friend trees. This is useful if trees of the same internal structure are compared. [Default: name of the tree]", default=[None])
+		
 		self.input_options.add_argument("-q", "--quantities", nargs="?", type="bool", default=False, const=True,
 		                                help="Print available quantities in given folder. [Default: %(default)s]")
 		self.input_options.add_argument("-x", "--x-expressions", type=str, nargs="+",
@@ -68,22 +72,11 @@ class InputRoot(inputfile.InputFile):
 	def prepare_args(self, parser, plotData):
 		super(InputRoot, self).prepare_args(parser, plotData)
 
-		if (plotData.plotdict["friend_treenames"] is not None) and (plotData.plotdict["friend_filenames"] is not None):
-			plotData.plotdict["friend_trees"] = []
-			for friend_treename, friend_filename in zip( 
-		                     	 plotData.plotdict["friend_treenames"],
-		                     	 plotData.plotdict["friend_filenames"]):
-				tuple = []
-				for plot_friend_treename, plot_friend_filename in zip(friend_treename.split(), friend_filename.split()):
-					tuple.append((plot_friend_treename, plot_friend_filename))
-				plotData.plotdict["friend_trees"].append(tuple)
-		else:
-			plotData.plotdict["friend_trees"] = [None]
-
-		self.prepare_list_args(plotData, ["nicks", "x_expressions", "y_expressions", "z_expressions", "x_bins", "y_bins", "z_bins", "scale_factors", "files", "directories", "folders", "weights", "friend_trees", "tree_draw_options"], help="InputRoot options")
+		self.prepare_list_args(plotData, ["nicks", "x_expressions", "y_expressions", "z_expressions", "x_bins", "y_bins", "z_bins", "scale_factors", "files", "directories", "folders", "weights", "friend_files", "friend_folders", "friend_aliases", "tree_draw_options"], help="InputRoot options")
 		inputbase.InputBase.prepare_nicks(plotData)
 		
-		plotData.plotdict["folders"] = [folders.split() if folders else [""] for folders in plotData.plotdict["folders"]]
+		for key in ["folders", "friend_files", "friend_folders"]:
+			plotData.plotdict[key] = [element.split() if element else element for element in plotData.plotdict[key]]
 		
 		if plotData.plotdict["read_config"]:
 			self.read_input_json_dicts(plotData)
@@ -104,7 +97,9 @@ class InputRoot(inputfile.InputFile):
 				y_bins,
 				z_bins,
 				nick,
-				friend_trees,
+				friend_files,
+				friend_folders,
+				friend_alias,
 				option
 		) in enumerate(pi.ProgressIterator(zip(
 				plotData.plotdict["files"],
@@ -117,7 +112,9 @@ class InputRoot(inputfile.InputFile):
 				plotData.plotdict["y_bins"],
 				plotData.plotdict["z_bins"],
 				plotData.plotdict["nicks"],
-				plotData.plotdict["friend_trees"],
+				plotData.plotdict["friend_files"],
+				plotData.plotdict["friend_folders"],
+				plotData.plotdict["friend_aliases"],
 				plotData.plotdict["tree_draw_options"]
 		), description="Reading ROOT inputs", visible=not self.hide_progressbar )):
 			# check whether to read from TTree or from TDirectory
@@ -137,7 +134,9 @@ class InputRoot(inputfile.InputFile):
 						y_bins=["25"] if y_bins is None else y_bins,
 						z_bins=["25"] if z_bins is None else z_bins,
 						weight_selection=weight, option=option, name=None,
-						friend_trees=friend_trees
+						friend_files=friend_files,
+						friend_folders=friend_folders,
+						friend_alias=friend_alias
 				)
 				
 			elif root_folder_type == "TDirectory":

HarryPlotter/python/utility/roottools.py
@@ -205,7 +205,8 @@ class RootTools(object):
 	def histogram_from_tree(self, root_file_names, path_to_trees,
 		                    x_expression, y_expression=None, z_expression=None,
 		                    x_bins=None, y_bins=None, z_bins=None,
-		                    weight_selection="", option="", name=None, friend_trees=None):
+		                    weight_selection="", option="", name=None,
+		                    friend_files=None, friend_folders=None, friend_alias=None):
 		"""
 		Read histograms from trees
 	
@@ -286,10 +287,6 @@ class RootTools(object):
 		tree = ROOT.TChain()
 		ROOT.SetOwnership(tree, False)
 		
-		
-		friend_tree = ROOT.TChain()
-		ROOT.SetOwnership(friend_tree, False)
-		
 		for root_file_name in root_file_names:
 			for path_to_tree in path_to_trees:
 				complete_path_to_tree = os.path.join(root_file_name, path_to_tree)
@@ -299,10 +296,19 @@ class RootTools(object):
 					log.error("Input %s does not contain any trees!" % complete_path_to_tree)
 		
 		#Add Friends
-		if friend_trees is not None:
-			for friend in friend_trees:
-				friend_tree.Add(friend[1] + "/" + friend[0])
-			tree.AddFriend(friend_tree)
+		friend_trees = []
+		if friend_files and friend_folders:
+			friend_trees.append(ROOT.TChain())
+			ROOT.SetOwnership(friend_trees[-1], False)
+			for root_file_name in friend_files:
+				for path_to_tree in friend_folders:
+					complete_path_to_tree = os.path.join(root_file_name, path_to_tree)
+					log.debug("Reading friend from ntuple %s ..." % complete_path_to_tree)
+					n_trees_added = friend_trees[-1].Add(complete_path_to_tree, -1)
+					if n_trees_added == 0:
+						log.error("Input %s does not contain any trees!" % complete_path_to_tree)
+			log.debug("ROOT.TTree.AddFriend(" + str(friend_trees[-1]) + ", \"" + (friend_alias if friend_alias else "") + "\")")
+			tree.AddFriend(friend_trees[-1], (friend_alias if friend_alias else ""))
 		
 		# ROOT optimisations
 		tree.SetCacheSize(256*1024*1024) # 256 MB

thomas-mueller/clipl

commit c7d4fd4bd4f4339e42a37467d11cfddfbc45da6b

inputroot.py: fix problem with empty folders introduced in c0d37abe5b06f1326df167e0345f397f3e26273c

HarryPlotter/python/input_modules/inputroot.py
@@ -76,7 +76,7 @@ class InputRoot(inputfile.InputFile):
 		inputbase.InputBase.prepare_nicks(plotData)
 		
 		for key in ["folders", "friend_files", "friend_folders"]:
-			plotData.plotdict[key] = [element.split() if element else element for element in plotData.plotdict[key]]
+			plotData.plotdict[key] = [([""] if element.empty() else element.split()) if element else [""] for element in plotData.plotdict[key]]
 		
 		if plotData.plotdict["read_config"]:
 			self.read_input_json_dicts(plotData)