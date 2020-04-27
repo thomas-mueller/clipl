7942ccdb30e43555d720c7a8c47ecb4063d6436a
Thomas Müller <t.muller@cern.ch>
2020-04-27 18:44:00 +0200

    remove unneeded scripts

--- clipl/scripts/ini_harry.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# This file might be merged with a general ini script
-
-source $ARTUSPATH/HarryPlotter/scripts/ini_harry_cmssw.sh
-
-#add harry to PATH
-export PATH=$ARTUSPATH/HarryPlotter/scripts:$PATH
